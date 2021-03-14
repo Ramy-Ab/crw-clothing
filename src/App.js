0a3ed55bfa010eb5ea1634e0ad13e97a3d0c34d2
ramy-ab <r.abadlia@esi-sba.dz>
2021-03-14 15:27:47 +0100

    add sign in

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import HomePage from './pages/homepage/homepage.component';
 import {Route,Switch,Link} from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () => (
 <div>
@@ -20,6 +21,7 @@ function App() {
       <Switch>
       <Route exact path='/' component={HomePage}/>
       <Route path='/shop' component={ShopPage}/>
+      <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
 
     </div>
