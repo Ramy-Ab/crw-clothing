1ac23f4d18bfedd0384d676f93349291b53411bd
ramy-ab <r.abadlia@esi-sba.dz>
2021-03-14 11:57:49 +0100

    add shop page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import {Route,Switch,Link} from 'react-router-dom';
+import ShopPage from './pages/shop/shop.component';
+
+const HatsPage = () => (
+<div>
+  <h1>HATS PAGE</h1>
+</div>
+);  
+
+
 
 function App() {
   return (
     <div >
-      <HomePage></HomePage> 
+      <Switch>
+      <Route exact path='/' component={HomePage}/>
+      <Route path='/shop' component={ShopPage}/>
+      </Switch>
+
     </div>
   );
 }
