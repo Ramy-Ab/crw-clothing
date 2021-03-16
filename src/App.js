b1d05bec1161c8972d2f41872ccea58b34618fd9
ramy-ab <r.abadlia@esi-sba.dz>
2021-03-16 15:35:15 +0100

    using redux

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import ShopPage from './pages/shop/shop.component';
 import Header from './component/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import React from 'react';
-import {auth} from './firebase/firebase.utils';
+import {auth,createUserProfileDocument} from './firebase/firebase.utils';
+import {connect} from 'react-redux';
+import  {setCurrentUser} from './redux/user/user.actions';
 
 const HatsPage = () => (
 <div>
@@ -17,19 +19,24 @@ const HatsPage = () => (
 
 
 class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      currentUser : null,
-    }
-  }
+
 
   unsubscribeFromAuth = null;
 
   componentDidMount(){
-    this.unsubscribeFromAuth =  auth.onAuthStateChanged(user =>{
-      this.setState({currentUser:user});
-      console.log(user);
+    const {setCurrentUser} = this.props;
+    this.unsubscribeFromAuth =  auth.onAuthStateChanged(async userAuth =>{
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot(snapshot => {
+              setCurrentUser({
+              id:snapshot.id,
+              ...snapshot.data()
+            });
+        });
+      }
+      setCurrentUser(userAuth);
     })
   }
   
@@ -40,7 +47,7 @@ class App extends React.Component {
   render(){
     return (
       <div >
-        <Header currentUser={this.state.currentUser}/>
+        <Header />
         <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop' component={ShopPage}/>
@@ -53,4 +60,8 @@ class App extends React.Component {
 
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser : user =>dispatch(setCurrentUser(user))
+});
+
+export default connect(null,mapDispatchToProps)(App);
---
+++ src/test.js
@@ -0,0 +1,6 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firestore = firebase.firestore();
+
+firestore.collection('users').doc('uj7wQzWvqUcHI935hqp7').collection('cartItems').doc('Q8gnP63KMNHPisLdONoR');
